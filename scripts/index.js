8646ba815eb4dd57111283bf422e52aaea1558b3
Sergei Denisenko <s.a.denisenko@gmail.com>
2025-03-12 23:02:32 +0300

    v1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
-// @todo: Темплейт карточки
+// Инициализация базовых переменных
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
 
-// @todo: DOM узлы
+// Функция создания карточки
+function createCard(cardData, deleteCallback) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
 
-// @todo: Функция создания карточки
+  // Устанавливаем данные карточки
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
 
-// @todo: Функция удаления карточки
+  // Добавляем обработчик удаления
+  deleteButton.addEventListener("click", () => deleteCallback(cardElement));
 
-// @todo: Вывести карточки на страницу
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
+
+// Вывести карточки на страницу
+function renderCards(cards) {
+  cards.forEach((card) => {
+    const cardElement = createCard(card, deleteCard);
+    placesList.append(cardElement);
+  });
+}
+
+// Вызов функции рендеринга
+renderCards(initialCards);
